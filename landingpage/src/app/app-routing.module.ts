03297db1a8ae7abb19bd5a3e49f8517d0f20240a
matheus dias <matheusmelhor3@outlook.com>
2022-06-14 15:56:40 -0300

    foi

--- landingpage/src/app/app-routing.module.ts
+++ landingpage/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{path: 'home', component: HomeComponent},
- {path: 'footer', component: FooterComponent}];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
