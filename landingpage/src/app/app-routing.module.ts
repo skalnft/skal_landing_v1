b72fc71b433c0b19f6aee600ddace3ffa4e2af9b
Guilherme Miserski <guilhermemiserski@hotmail.com>
2022-06-14 14:06:44 -0300

    adicionando bootstrap

--- landingpage/src/app/app-routing.module.ts
+++ landingpage/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'home', component: HomeComponent},
+ {path: 'footer', component: FooterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
