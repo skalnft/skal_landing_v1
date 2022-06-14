7c77043e750611aa7c5b6c1aed2f095be89457f1
Guilherme Miserski <guilhermemiserski@hotmail.com>
2022-06-14 16:00:01 -0300

    rota

--- landingpage/src/app/app-routing.module.ts
+++ landingpage/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -8,14 +7,6 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [{path: 'home', component: HomeComponent},
  {path: 'footer', component: FooterComponent},
 {path: 'about', component: AboutComponent}];
-=======
-import { HomeComponent } from './home/home.component';
-
-const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-];
->>>>>>> 03297db1a8ae7abb19bd5a3e49f8517d0f20240a
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
