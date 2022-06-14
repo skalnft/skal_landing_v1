d2640e19dbd26dd7f46d51e801f7311ad2b1dbdd
Guilherme Miserski <guilhermemiserski@hotmail.com>
2022-06-14 15:56:54 -0300

    adicionando o about

--- landingpage/src/app/app-routing.module.ts
+++ landingpage/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [{path: 'home', component: HomeComponent},
- {path: 'footer', component: FooterComponent}];
+ {path: 'footer', component: FooterComponent},
+{path: 'about', component: AboutComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
