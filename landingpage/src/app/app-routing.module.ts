8cae13c0b3c3fab3e32807249e9ce5c2c8837a4d
Guilherme Miserski <guilhermemiserski@hotmail.com>
2022-06-15 20:29:01 -0300

    arrumando rota

--- landingpage/src/app/app-routing.module.ts
+++ landingpage/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{path: 'home', component: HomeComponent},
- {path: 'footer', component: FooterComponent},
-{path: 'about', component: AboutComponent}];
+const routes: Routes = [{ path: 'home', component: HomeComponent },
+{ path: 'footer', component: FooterComponent },
+{ path: 'about', component: AboutComponent }, 
+{ path: '', redirectTo: '/home', pathMatch: 'full' }];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
